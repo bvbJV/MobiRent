8bd6db3efe0e7d92c9e51c8254df3328964cbe31
belharrak <belharrak@alumnat.copernic.cat>
2026-03-09 10:54:43 +0100

    Rutas

--- mobile/app/src/main/java/cat/copernic/appvehicles/MainActivity.kt
+++ mobile/app/src/main/java/cat/copernic/appvehicles/MainActivity.kt
@@ -20,7 +20,7 @@ class MainActivity : ComponentActivity() {
 
 
         val retrofit = Retrofit.Builder()
-            .baseUrl("http://192.168.1.210:8080/api/")
+            .baseUrl("http://192.168.1.91:8080/api/")
             .addConverterFactory(GsonConverterFactory.create())
             .build()
 
